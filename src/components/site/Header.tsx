06642771f9c346658b4409bc1dd4a1f57def7d13
rahulbansalacsius <158162410+rahulbansalacsius@users.noreply.github.com>
2026-09-10 13:47:57 +0530

    Update Header.tsx

--- src/components/site/Header.tsx
+++ src/components/site/Header.tsx
@@ -440,6 +440,15 @@ export function Header() {
           </div>
         )}
       </div>
+      <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KY78N96JBN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KY78N96JBN');
+</script>
     </header>
   );
 }
